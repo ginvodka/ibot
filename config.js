9649cfee6fbcb1ce95b712a8d2639f7af3e988e8
marco <iatestmarco@gmail.com>
2017-03-29 03:01:00 +0200

    config.js commit

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    FB_PAGE_TOKEN: '',
+    FB_VERIFY_TOKEN: '',
+    API_AI_CLIENT_ACCESS_TOKEN: '',
+    FB_APP_SECRET: '',
+    SERVER_URL: "",
+    // email configuration
+    // SENGRID_API_KEY: '',
+    // EMAIL_FROM: '',
+    // EMAIL_TO: '',
+    WEATHER_API_KEY: '',
+    NEO4J_URL: "",
+};
